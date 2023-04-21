af5ffee98234c6f2e1bf303a796700226e7bfd89
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-21 14:53:47 +0300

    Added "nav variants" framer motion amd styled Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,10 +3,11 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
-    variants={}
+    variants={navVariants}
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
