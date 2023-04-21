524b6fb85f3a4d1f7118d570e3d5fb887f1a6577
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-21 20:11:50 +0300

    Added "Hero" section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { Navbar } from '../components';
+import { Hero } from '../sections';
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
+    <Hero />
   </div>
 );
 
