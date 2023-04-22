918f3ba3bf8dcd1e44b5c0bed9562a5d2137d177
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-22 20:43:46 +0300

    Added "New features" section

--- components/index.js
+++ components/index.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
 import { TypingText, TitleText } from './CustomTexts';
 import ExploreCard from './ExploreCard';
+import NewFeatures from './NewFeatures';
 
 export {
   Navbar,
   TitleText,
   TypingText,
+  NewFeatures,
   ExploreCard
 };
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion';
 
 import styles from '../styles';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { TitleText, TypingText } from '../components';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { newFeatures } from '../constants';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -21,6 +22,11 @@ const WhatsNew = () => (
       >
         <TypingText title="| Whats new?" />
         <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
       </motion.div>
 
       <motion.div
