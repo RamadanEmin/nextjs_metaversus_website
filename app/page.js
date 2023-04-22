32650e99b961f8a73226515e3b55851c98b47c46
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-22 09:49:23 +0300

    Added "Get started" section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import { Navbar } from '../components';
 import {
   About,
   Explore,
+  GetStarted,
   Hero
 } from '../sections';
 
@@ -13,6 +14,9 @@ const Page = () => (
       <About />
       <div className="gradient-03 z-0" />
       <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
     </div>
   </div>
 );
--- sections/index.js
+++ sections/index.js
@@ -1,9 +1,11 @@
 import Hero from './Hero';
 import About from './About';
 import Explore from './Explore';
+import GetStarted from './GetStarted';
 
 export {
   Hero,
   About,
-  Explore
+  Explore,
+  GetStarted
 };
